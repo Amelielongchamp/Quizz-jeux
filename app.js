855a7dc56bc7a99acc270e1baf75a2d16682f0b1
Amelielongchamp <amelie.longchamp42400@gmail.com>
2025-05-14 12:58:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,41 +13,72 @@
 //  via un prompt "question"
 //3:Creer une boucle qui compare la réponse utilisateur à la réponse attendue.
 //4:Creer une fonction afficher 
-//5: Creer une fonction question suivante 
 
 
 //1: Creer 1 tableau réponses creer 1 tableau question.
-let tableauQuestion=[{question:[
-    { q1:"Quel est le panneau piste cyclable?",
-      r1:"a"},
-     {q2:"Le panneau est une obligation de ?",
-        r2:"c"},
-    {q3:" Dans cette situation etes vous prioritaires?",
-      r3:"non"},
-    {q4:"Sans présence de panneaux de vitesse à combien êtes vous autorisés à rouler?",
-      r4:"80km"},
-    { q5:"Quel est le mot manquant sur le panneau?",
-        r5:"gendarmerie"
-    } 
-]}
-]
+let tableauQuestion = [
+  {
+    q: "Quel est le panneau piste cyclable ?",
+    a: "panneau A",
+    b: "panneau B",
+    c: "panneau C",
+    d: "panneau D",
+    r: "a"
+  },
+  {
+    q: "Le panneau est une obligation de ?",
+    a: "tourner a droite",
+    b: "tourner a gauche",
+    r: "b"
+  },
+  {
+    q: " Dans cette situation etes vous prioritaires?",
+    a: "oui",
+    b: "non",
+    r: "non"
+  },
+  {
+    q: "Sans présence de panneaux de vitesse à combien êtes vous autorisés à rouler?",
+    a: "70mk/h",
+    b: "80km/h",
+    c: "90km/h",
+    d: "100km/h",
+
+    r: "b"
+  },
+  {
+    q: "Quel est le mot manquant sur le panneau?",
+    a: "gendarmerie",
+    b: "garderie",
+    c: "prison",
+
+    r: "a"
+  }]
 
 //2: Creer une variable qui s'appelle réponse stock la reponse utilisateur
 //  via un prompt "question"
-letReponse=prompt(tableauQuestion)
 
-//3:Creer une boucle qui compare la réponse utilisateur à la réponse attendue.
+//3:Creer une boucle qui compare la réponse utilisateur à la réponse attendue+afficher
 
-function Quizz(question){
-tableauQuestion.forEach(question => {
-    if(reponse===question[1]){
+function Quizz(question) {
+  tableauQuestion.forEach(question => {
+    let reponse = prompt(`${question.q} : \n
+    a: ${question.a} \n
+    b: ${question.b}\n
+    c:${question.c}\n
+    d:${question.d}  
+    `)
 
-console.log("bravo!")
+    if (reponse == question.r) {
+
+      alert("bravo!")
     }
-     else{
+    else {
 
-console.log("c'est faux!")
+      alert("c'est faux!")
     }
-    
-});
+
+  });
 }
+
+Quizz()
